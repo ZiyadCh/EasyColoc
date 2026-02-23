1a09b88f56edbbfff45e4f0385343adb95ee426d
ZiyadCh <chetoziyad@gmail.com>
2026-02-23 15:45:43 +0000

    created migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->enum('role', ['admin','owner','member'])->default('member');
+            $table->integer('reputation');
+            $table->boolean('active');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
