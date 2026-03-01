737e457de137203cc64aaeda6ef2544e8417e764
ZiyadCh <chetoziyad@gmail.com>
2026-03-01 00:55:21 +0000

    left at trying to divide the dette agter user is kicked out

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -11,9 +11,21 @@ class OwnerController extends Controller
     public function retirerUser($id): RedirectResponse
     {
         $user = User::findOrFail($id);
+        $colocation = $user->colocations()->first();
         if ($user->id == auth()->user()->id) {
             return redirect()->back();
         }
+        //if user has dette
+        if ($user->dette > 0) {
+            $user->decrement('reputation');
+            $autre = $colocation->users->where('id', '!=', $user->id);
+            $prix = $user->dette / $autre->count();
+            //divide the money
+            foreach ($autre as $a) {
+                $a->increment('dette', $prix);
+            }
+        }
+
         $user->colocations()->detach();
         return redirect()->back();
     }
